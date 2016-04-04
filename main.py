2d6afe2418b240c52785c1ef5f1e2ce3bef25e0c
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2016-04-03 22:58:30 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+###############################################
+# youtube frame################3
+## <iframe width="560" height="315" src="https://www.youtube.com/embed/XTTu0k1H2JI?start=30&amp;end=55&amp;version=3" frameborder="0"></iframe>
+
+# youtube thumbnail 0 is large, 1-3 small
+#http://img.youtube.com/vi/<insert-youtube-video-id-here>/[0-3].jpg
+
+# dectect if valid video id
+# https://www.googleapis.com/youtube/v3/videos?id=<videoID>&key=<API_KEY>&part=id
+####################################################
+
+
+from credentials import API_KEY
+import webapp2
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
