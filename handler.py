821d0b0b7dea4328c1ee4acec0a8359dc95e5319
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2016-04-04 18:18:32 -0700

    handler class with render method

---
+++ handler.py
@@ -0,0 +1,15 @@
+import webapp2
+import jinja2
+import os
+
+class handler(webapp2.RequestHandler):
+    JINJA_ENVIRONMENT = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
+        extensions=['jinja2.ext.autoescape'],
+        autoescape=True)
+
+    def render(self, template, var=None):
+        self.response.headers['Content-Type'] = 'text/html'
+        jTemplate = self.JINJA_ENVIRONMENT.get_template(template)
+        if var :self.response.write(jTemplate.render(var))
+        else: self.response.write(jTemplate.render())
