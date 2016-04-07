1f8982114b2f721cae344b8446021024aa200748
Brandon Swanson <swanyriver@gmail.com>
2016-04-06 21:04:45 -0700

    debug of edit post handling

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 ## <iframe width="560" height="315" src="https://www.youtube.com/embed/XTTu0k1H2JI?start=30&amp;end=55&amp;version=3" frameborder="0"></iframe>
 
 # youtube thumbnail 0 is large, 1-3 small
-#http://img.youtube.com/vi/<insert-youtube-video-id-here>/[0-3].jpg
+# http://img.youtube.com/vi/<insert-youtube-video-id-here>/[0-3].jpg
 
 # dectect if valid video id
 # https://www.googleapis.com/youtube/v3/videos?id=<videoID>&key=<API_KEY>&part=id
@@ -18,6 +18,7 @@ import models
 import credentials
 import json
 from google.appengine.api import urlfetch
+from google.appengine.ext import ndb
 
 
 class MainHandler(Handler):
@@ -45,23 +46,24 @@ class addHandler(Handler):
         if not playlist:
             return self.redirect('/?' + Handler.warning("Playlist not found"))
         self.render("add.html", var={"playlist": playlist})
+
     def post(self):
         playlist = models.Playlist.getPlaylistFromRequest(self.request)
 
         if not playlist:
             return self.redirect("/?" + Handler.warning("Playlist not found"))
-        if any((k not in self.request.POST or not self.request.POST[k] for k in ("title", "videoID", "startTime", "endTime"))):
+        if any((k not in self.request.POST or not self.request.POST[k] for k in
+                ("title", "videoID", "startTime", "endTime"))):
             return self.redirect('/add/?' + Handler.warning("required fields not found") + '&' + playlist.keyForLink())
-
 
         self.response.write(models.getPopulateDictionary(models.Snippet, self.request.POST.items()))
 
         # validate videoID
         # add part=id,snippet to get title from snippet object
-        result = urlfetch.fetch("https://www.googleapis.com/youtube/v3/videos?id=%s&key=%s&part=id"%(
+        result = urlfetch.fetch("https://www.googleapis.com/youtube/v3/videos?id=%s&key=%s&part=id" % (
             self.request.get('videoID'), credentials.API_KEY))
         if result.status_code != 200:
-            return self.redirect("/add/?%s&%s"%
+            return self.redirect("/add/?%s&%s" %
                                  (playlist.keyForLink(), Handler.warning("Youtube unreachable to verify videoID")))
 
         videoJSON = json.loads(result.content)
@@ -79,7 +81,7 @@ class addHandler(Handler):
         playlist.put()
 
         # redirect to view with playlist and status
-        return self.redirect("/view/?%s&%s"%
+        return self.redirect("/view/?%s&%s" %
                              (playlist.keyForLink(), Handler.status("Snippet added to playlist")))
 
 
@@ -109,17 +111,40 @@ class delSnippetHandler(PlaylistHandler):
 
         self.redirect("/view/?" + playlist.keyForLink())
 
+
 class editHandler(PlaylistHandler):
     def getPlaylist(self, playlist):
         self.render("editPlaylist.html", {'playlist': playlist})
 
     def post(self):
+        playlist = models.Playlist.getPlaylistFromRequest(self.request)
+        if not playlist:
+            return self.redirect("/?" + Handler.warning("Playlist not found"))
+
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write("%s\n"%self.request.POST)
-        self.response.write("%s\n"%self.request.get_all('key'))
-        #for key in self.request.get_all('key'):
+        self.response.write("%s\n" % self.request.POST)
+        self.response.write("%s\n" % self.request.get_all('key'))
+        for key in self.request.get_all('key'):
+            d = {k: int(self.request.get(key + '_' + k)) if isinstance(
+                                                             getattr(models.Snippet, k),
+                                                             ndb.IntegerProperty) else
+                self.request.get(key+'_'+k) for k in models.Snippet._properties}
+            self.response.write("snippet %s: %s\n" % (key, d))
+            # self.response.write("snippet %s: %s" % (key,
+            #                                         {k: int(self.request.get(key + '_' + k)) if isinstance(
+            #                                             getattr(models.Snippet, k),
+            #                                             ndb.IntegerProperty) else self.request.get(key + '_' + k) for k
+            #                                          in models.Snippet._properties}))
 
+        self.response.write("\n--------------before--------------------------------\n%s\n--------------------------------\n\n"%playlist)
+        playlist.snippets = [models.Snippet(**{
+            k: int(self.request.get(key + '_' + k)) if isinstance(getattr(models.Snippet, k),
+                                                              ndb.IntegerProperty) else self.request.get(key + '_' + k)
+            for k in models.Snippet._properties}) for key in self.request.get_all('key')]
+        #playlist.put()
+        #return self.redirect("/view/?" + playlist.keyForLink())
 
+        self.response.write("\n--------------after--------------------------------\n%s\n--------------------------------\n\n"%playlist)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
